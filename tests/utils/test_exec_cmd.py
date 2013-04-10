18d6f10a0d911ee4142ff9a1ef510f27ffe45eac
elias <elias@egsdata.dk>
2013-04-10 10:25:24 +0200

    Add test of exec_cmd

---
+++ tests/utils/test_exec_cmd.py
@@ -0,0 +1,21 @@
+import unittest
+from opbeatcli.utils.exec_cmd import exec_cmd
+
+class TestExecCmd(unittest.TestCase):
+    def test_empty(self):
+        output = exec_cmd('tail /dev/null')
+        # Check if empty
+        self.assertEqual(next(output, None), None)
+    def test_error(self):
+        with self.assertRaises(OSError):
+            exec_cmd('this_is_not_a_valid_command')
+    def test_invalid(self):
+        output = exec_cmd('ls this_is_not_a_file')
+        self.assertEqual(next(output, None), None)
+    def test_correct(self):
+        output = exec_cmd('printf test')
+        result = ''
+        for line in output:
+            result += line
+
+        self.assertEqual(result, 'test')
