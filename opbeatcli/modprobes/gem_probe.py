6a676989eae8cf3ab46aaaefdc006a18474ed373
elias <elias@egsdata.dk>
2013-04-10 13:16:15 +0200

    Refactor gem_probe

--- opbeatcli/modprobes/gem_probe.py
+++ opbeatcli/modprobes/gem_probe.py
@@ -1,15 +1,14 @@
-import subprocess
 import re
+from opbeatcli.utils.exec_cmd import exec_cmd
 from opbeatcli.modprobes.release import (Module, Release)
 
 def run():
-	releases = []
-	try:
-		p = subprocess.Popen(['gem', 'list'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-		for line in p.stdout:
-			if line != '' and not re.match('^[*]', line):
-				pkg = re.split('\s|[()]', line)
-				mod = Module(name=pkg[0])
-				releases.append(Release(mod, version=pkg[2]))
-	finally:
-		return releases
+    releases = []
+    output = exec_cmd('gem list')
+
+    for line in output:
+        if line != '' and not re.match('^[*]', line):
+            pkg = re.split('\s|[()]', line)
+            mod = Module(name=pkg[0])
+            releases.append(Release(mod, version=pkg[2]))
+    return releases
