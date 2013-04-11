5ce84157d2b074ce5156114784fe867b5bad7816
elias <elias@egsdata.dk>
2013-04-11 11:27:47 +0200

    Add current working directory param to exec_cmd

--- opbeatcli/utils/exec_cmd.py
+++ opbeatcli/utils/exec_cmd.py
@@ -1,10 +1,10 @@
 import subprocess
 import re
-def exec_cmd(command):
-    """Return the STDOUT as a open file object"""
+def exec_cmd(command, diretory=None):
+    """Return the STDOUT as a open file object."""
     args = re.split('\s+', command)
     try:
-        p = subprocess.Popen(args, stdout=subprocess.PIPE)
+        p = subprocess.Popen(args, stdout=subprocess.PIPE, cwd=diretory)
     except OSError:
         return iter([])
     else:
--- tests/utils/test_exec_cmd.py
+++ tests/utils/test_exec_cmd.py
@@ -6,16 +6,21 @@ class TestExecCmd(unittest.TestCase):
         output = exec_cmd('tail /dev/null')
         # Check if empty
         self.assertEqual(next(output, None), None)
+
     def test_error(self):
         output = exec_cmd('this_is_not_a_valid_command')
         self.assertEqual(next(output, None), None)
+
     def test_invalid(self):
         output = exec_cmd('ls this_is_not_a_file')
         self.assertEqual(next(output, None), None)
+
     def test_correct(self):
         output = exec_cmd('printf test')
-        result = ''
-        for line in output:
-            result += line
+        result = ''.join(output)
+        self.assertEqual(result, 'test')
 
+    def test_working_dir(self):
+        output = exec_cmd('cat exec_cmd', 'tests/utils/fixtures')
+        result = ''.join(output)
         self.assertEqual(result, 'test')
