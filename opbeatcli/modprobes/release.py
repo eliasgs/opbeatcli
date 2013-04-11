7857bdf6cd9f6142f8dc0cf9295d9cbcff683865
elias <elias@egsdata.dk>
2013-04-11 14:29:21 +0200

    Adhere to PEP 8 - add blank lines

--- opbeatcli/modprobes/release.py
+++ opbeatcli/modprobes/release.py
@@ -1,8 +1,10 @@
 class Module(object):
     def __init__(self, name):
         self.name = name
+
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
+
 
 class Vcs(object):
     def __init__(self, vcs_type, revision, repository, branch):
@@ -10,13 +12,16 @@ class Vcs(object):
         self.revision = revision
         self.repository = repository
         self.branch = branch
+
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
+
 
 class Release(object):
     def __init__(self, module, version=None, vcs=None):
         self.module = module
         self.version = version
         self.vcs = vcs
+
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
