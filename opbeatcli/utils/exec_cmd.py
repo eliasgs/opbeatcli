cef4a7c7120cb8206af730611dd3cb18c8f3f0f9
Elias G. Schneevoigt <eliasgs@gmail.com>
2013-04-29 12:54:16 +0200

    Ignore stderr messages in exec_cmd
    Redirect stderr message to /dev/null since exec_cmd is a internal
    implementation detail, and users of the cli should not be concerned with
    e.g. not having npm installed.

--- opbeatcli/utils/exec_cmd.py
+++ opbeatcli/utils/exec_cmd.py
@@ -1,10 +1,14 @@
 import subprocess
+import os
 import re
 def exec_cmd(command, diretory=None):
     """Return the STDOUT as a open file object."""
     args = re.split('\s+', command)
     try:
-        p = subprocess.Popen(args, stdout=subprocess.PIPE, cwd=diretory)
+        p = subprocess.Popen(args,
+                             stdout=subprocess.PIPE,
+                             stderr=open(os.devnull, 'w'),
+                             cwd=diretory)
     except OSError:
         return iter([])
     else:
